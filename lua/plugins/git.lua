9e2bacd1938293ae208e71e13781e52c8ac23c8e
superunyx <jayvantsolanki74@gmail.com>
2025-09-08 00:13:40 +0530

    Readmeupdate

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -17,11 +17,11 @@ return {
             -- Set a vim motion to <Space> + g + <Shift>A to all files changed to the staging area
             vim.keymap.set("n", "<leader>gA", ":Git add .<cr>", {desc = "[G]it Add [A]ll"})
             -- Set a vim motion to <Space> + g + a to add the current file and changes to the staging area
-            vim.keymap.set("n", "<leader>ga", ":Git add %<cr>", {desc = "[G]it [A]dd"})
+            vim.keymap.set("n", "<leader>ga", "Git add", {desc = "[G]it [A]dd"})
             -- Set a vim motion to <Space> + g + c to commit the current chages
             vim.keymap.set("n", "<leader>gc", ":Git commit", {desc = "[G]it [C]ommit"})
             -- Set a vim motion to <Space> + g + p to push the commited changes to the remote repository
-            vim.keymap.set("n", "<leader>gp", ":Git push", {desc = "[G]it [P]ush"})
+            vim.keymap.set("n", "<leader>gp", "Git push", {desc = "[G]it [P]ush"})
         end
     }
 }
